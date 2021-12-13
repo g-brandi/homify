cbd548acc371144b9201b7afdc9be64c9f52e2eb
davide-abbattista <davide27abbattista@gmail.com>
2021-12-13 12:07:10 +0100

    Definizione iniziale delle activity GoogleSignInActivity, dopo aver inserito l'ID client OAuth 2.0 del server di backend

--- app/src/main/java/com/example/homify/EmailPasswordActivity.java
+++ app/src/main/java/com/example/homify/EmailPasswordActivity.java
@@ -23,7 +23,7 @@ public class EmailPasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_email_password);
 
         // [START initialize_auth]
         // Initialize Firebase Auth
