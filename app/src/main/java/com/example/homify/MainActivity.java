d5f4f885759b1021d8425cdbdeab598d85494d2c
davide-abbattista <davide27abbattista@gmail.com>
2021-12-13 12:48:25 +0100

    .

--- app/src/main/java/com/example/homify/MainActivity.java
+++ app/src/main/java/com/example/homify/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.homify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Button btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        btnGoogle = findViewById(R.id.btnGoogle);
+//        btnGoogle.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, GoogleSignInActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
