9c743b782c752ca2f02b7e0dcd9420c561de906c
davide-abbattista <davide27abbattista@gmail.com>
2021-12-13 14:50:12 +0100

    rivisita activity per login

--- app/src/main/java/com/example/homify/MainActivity.java
+++ app/src/main/java/com/example/homify/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnGoogle;
+    Button btnEmailPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnGoogle = findViewById(R.id.btnGoogle);
+        btnEmailPassword = findViewById(R.id.btnEmailPassword);
+
         btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GoogleSignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEmailPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EmailPasswordActivity.class);
                 startActivity(intent);
             }
         });
