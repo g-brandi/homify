ab572a34a22a5de8768c373815be377740139713
davide-abbattista <davide27abbattista@gmail.com>
2021-12-13 14:20:49 +0100

    bottone di logout nell'activity user home con relativo onClickListener

--- app/src/main/java/com/example/homify/UserHomeActivity.java
+++ app/src/main/java/com/example/homify/UserHomeActivity.java
@@ -2,13 +2,32 @@ package com.example.homify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserHomeActivity extends AppCompatActivity {
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(UserHomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
