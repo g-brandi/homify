8ca16563d6257d88260dbb64019b4f033dde20c2
g.brandi <g.brandi@studenti.poliba.it>
2021-12-16 09:46:39 +0100

    Ho aggiunto il grafico relativo alla temperatura

--- app/src/main/java/com/example/homify/Sensor1Activity.java
+++ app/src/main/java/com/example/homify/Sensor1Activity.java
@@ -14,7 +14,6 @@ public class Sensor1Activity extends AppCompatActivity {
 
         WebView webView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
-
         webSettings.setJavaScriptEnabled(true);
 
         webView.loadUrl("https://thingspeak.com/channels/1595381/charts/2?bgcolor=%23ffffff&color=%23d62020&days=10&dynamic=true&median=60&title=Umidit%C3%A0&type=spline&xaxis=Tempo&yaxis=Umidit%C3%A0+%28%25%29");
