7670d9f52b25114af5b0497a4c11e78ba1270480
davide-abbattista <davide27abbattista@gmail.com>
2021-12-18 12:51:16 +0100

    Aggiornamento schermate di login

--- app/src/main/java/com/example/homify/MainActivity.java
+++ app/src/main/java/com/example/homify/MainActivity.java
@@ -1,97 +1,32 @@
 package com.example.homify;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnGoogle;
-    private Button btnEmailPassword;
+    private Button btnLogin;
     private Button btnRegister;
-
-    private TextView email;
-    private TextView password;
-
-    private static final int RC_SIGN_IN = 9001;
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance("https://homify-is07-default-rtdb.europe-west1.firebasedatabase.app/");
-    DatabaseReference myRef = database.getReference();
-
-    // [START declare_auth]
-    private FirebaseAuth mAuth;
-    // [END declare_auth]
-
-    private GoogleSignInClient mGoogleSignInClient;
-
-    private Utente user;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnGoogle = findViewById(R.id.btnGoogle);
-        btnEmailPassword = findViewById(R.id.btnEmailPassword);
+        btnLogin = findViewById(R.id.btnEmailPassword);
         btnRegister = findViewById(R.id.btnRegister);
-        email = findViewById(R.id.txtEmail);
-        password = findViewById(R.id.txtPassword);
 
-        // [START config_signin]
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        // [END config_signin]
-
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
-
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn();
-            }
-        });
-
-        btnEmailPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    signIn(email.getText().toString(), password.getText().toString());
-                } catch (java.lang.IllegalArgumentException exception) {
-                    Toast.makeText(MainActivity.this, "Complilare entrambe le celle", Toast.LENGTH_SHORT).show();
-                }
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -103,107 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    // [START on_start_check_user]
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-    // [END on_start_check_user]
-
-    // [START onactivityresult]
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d("GoogleLogin", "firebaseAuthWithGoogle:" + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w("GoogleLogin", "Google sign in failed", e);
-                Toast.makeText(this, "Accesso non eseguito", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    // [END onactivityresult]
-
-    // [START auth_with_google]
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("GoogleLogin", "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                            insertOnDatabaseForGmail();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("GoogleLogin", "signInWithCredential:failure", task.getException());
-                            updateUI(null);
-                        }
-                    }
-                });
-    }
-    // [END auth_with_google]
-
-    // [START signin]
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-    // [END signin]
-
-    private void signIn(String email, String password) {
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("EmailPasswordLogin", "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("EmailPasswordLogin", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "La password è errata o non ci sono utenti registrati con questa email",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-        // [END sign_in_with_email]
-    }
-
-    private void updateUI(FirebaseUser user) {
-        if(user!=null){
-            Intent intent = new Intent(getApplicationContext(),UserHomeActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    private void insertOnDatabaseForGmail() {
-        String var = myRef.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).toString();
-        String var1 = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
-        if (!var.equals("https://homify-is07-default-rtdb.europe-west1.firebasedatabase.app/users/" + var1)) {
-            user = new Utente(FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), FirebaseAuth.getInstance().getCurrentUser().getEmail());
-            myRef.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user);
-        }
     }
 }
 
